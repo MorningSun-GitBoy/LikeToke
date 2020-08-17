4f655f42b7d27790c0bea4f5f6c7eade7f4e29c4
Bachelor <AndyWolfCF2013@163.com>
2020-08-17 22:45:14 +0800

    add plugins and denpendencies , try to run on tomcat

--- LikeToke-maven/src/main/java/com/lingxian/servlet/SigninServlet.java
+++ LikeToke-maven/src/main/java/com/lingxian/servlet/SigninServlet.java
@@ -1,14 +1,19 @@
 package com.lingxian.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class SigninServlet extends javax.servlet.http.HttpServlet {
+@WebServlet(name = "SigninServlet",value = "/signin")
+public class SigninServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-
+        resp.setCharacterEncoding("gbk");
+        resp.getWriter().println("servlet run nomally");
+        resp.flushBuffer();
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
